53abc163e281bf3484584c62bf3c6c1c9c70a446
qsheeeeen <kshz2155757@gmail.com>
2017-05-30 20:10:35 +0800

    Use RC ESC

--- environment/racing_car.py
+++ environment/racing_car.py
@@ -10,22 +10,20 @@ import pigpio
 
 
 class RacingCar(object):
-    def __init__(self, use_controller=False, use_rc_motor=False):
-        self.__use_controller = use_controller
-        self.__use_rc_motor = use_rc_motor
-
-        self.__servo_pin = 6
-        self.__motor_forward_pin = 12
-        self.__motor_backward_pin = 13
+    def __init__(self):
+        self.___spoiler_servo_pin = 5
+        self.__steering_servo_pin = 4
+        self.__motor_pin = 17
         self.__left_line_sensor_pin = 1
         self.__right_line_sensor_pin = 2
-        self.__encoder_dir_pin = 5
+        self.__encoder_dir_pin = 12
+        self.__encoder_zero_pin = 13
 
         self.__forward_pin_level = 1
 
         image_width, image_height = (320, 240)
 
-        self.image = np.empty((image_height, image_width, 3), dtype=np.uint8)
+        self.__image = np.empty((image_height, image_width, 3), dtype=np.uint8)
 
         self.__cam = picamera.Picamera()
         self.__cam.resolution = image_width, image_height
@@ -38,166 +36,63 @@ class RacingCar(object):
 
         self.__pi_car.callback(self.__left_line_sensor_pin, pigpio.RISING_EDGE, self.__line_interrupt_handle)
         self.__pi_car.callback(self.__right_line_sensor_pin, pigpio.RISING_EDGE, self.__line_interrupt_handle)
+        self.__pi_car.callback(self.__encoder_zero_pin, pigpio.RISING_EDGE, self.__update_reward)
 
-        if self.__use_rc_motor:
-            self.__pi_car.set_servo_pulsewidth(self.__motor_forward_pin, 0)
-        else:
-            self.__pi_car.set_PWM_frequency(self.__motor_forward_pin, 10000)
-            self.__pi_car.set_PWM_frequency(self.__motor_backward_pin, 10000)
+        self.__pi_car.set_servo_pulsewidth(self.__motor_pin, 0)
+        self.__pi_car.set_servo_pulsewidth(self.__steering_servo_pin, 0)
+        self.__pi_car.set_servo_pulsewidth(self.___spoiler_servo_pin, 0)
 
-            self.__pi_car.set_PWM_dutycycle(self.__motor_forward_pin, 0)
-            self.__pi_car.set_PWM_dutycycle(self.__motor_backward_pin, 0)
-
-        self.__pi_car.set_servo_pulsewidth(self.__servo_pin, 0)
+        self.__motor = 0.
+        self.__steering_angle = 0.
 
         self.__reward = 0.
         self.__done = False
-        self.__info = [''] * 10
+        self.__info = (self.__motor, self.__steering_angle)
 
-        self.__gas_duty_cycle = 0.
-        self.__break_duty_cycle = 0.
-        self.__steering = 0.
+        self.action_list = (
+            'Left & Right',
+            'Gas & Break'
+        )
 
-        if self.__use_controller:
-            self.action_list = (
-                'Gas ',
-                'Break',
-                'Left & Right'
-            )
-            self.action_space = (2,)
-        else:
-            self.action_list = (
-                'None',
-                'Gas',
-                'Break',
-                'Turn left',
-                'Turn right',
-                'Gas & Turn left',
-                'Gas & Turn right',
-                'Break & Turn left',
-                'Break & Turn right'
-            )
+        self.action_space = np.zeros(len(self.action_space), np.float32)
 
-            self.action_space = len(self.action_list)
-
-    def reset(self) -> (np.ndarray, int, bool, list):
-        """Reset environment
-
-        Returns: state, reward, done, info.
-
-        """
+    def reset(self) -> (np.ndarray, float, bool, list):
         self.__get_image()
-        self.__reward.value = 0
-        self.__done.value = False
-        self.__info.value = (self.__gas_duty_cycle, self.__break_duty_cycle, self.__steering)
+        self.__reward = 0
+        self.__done = False
 
-        return self.image, self.__reward, self.__done, self.__info
+        return self.__image, self.__reward, self.__done, self.__info
 
-    def step(self, action: np.ndarray) -> (np.ndarray, int, bool, list):
-        """
+    def step(self, action: np.ndarray) -> (np.ndarray, float, bool, list):
+        self.__steering_angle = action[0]
+        self.__motor = action[1]
 
-        Args:
-            action: Actions to perform.
+        real_pwm = self.scale_range(self.__motor, -1., 1., 1000., 2000.)
+        self.__pi_car.set_servo_pulsewidth(self.__motor_pin, real_pwm)
+        self.__pi_car.set_servo_pulsewidth(self.___spoiler_servo_pin, real_pwm)
 
-        Returns: state, reward, done, info.
-
-        """
-        if self.__use_controller:
-            assert action.shape == (3,), 'Action must contains 3 number.'
-            self.__steering = -action[0]
-            self.__gas_duty_cycle = action[1]
-            self.__break_duty_cycle = action[2]
-
-        else:
-            index = np.argmax(action)
-            print('Perform action:' + self.action_list[index])
-            if index == 0:
-                pass
-
-            elif index == 1:
-                self.__gas_duty_cycle += 0.1
-                self.__break_duty_cycle = 0.
-
-            elif index == 2:
-                self.__gas_duty_cycle = 0.
-                self.__break_duty_cycle += 0.1
-
-            elif index == 3:
-                self.__steering -= 0.1
-
-            elif index == 4:
-                self.__steering += 0.1
-
-            if self.__gas_duty_cycle > 1.:
-                self.__gas_duty_cycle = 1.
-
-            elif self.__gas_duty_cycle < 0.:
-                self.__gas_duty_cycle = 0.
-
-            if self.__break_duty_cycle > 1.:
-                self.__break_duty_cycle = 1.
-
-            elif self.__break_duty_cycle < 0.:
-                self.__break_duty_cycle = 0.
-
-            if self.__steering > 1.:
-                self.__steering = 1.
-
-            elif self.__steering < -1.:
-                self.__steering = -1.
-
-        if self.__use_rc_motor:
-            pass
-
-        else:
-            real_pwm = self.scale_range(self.__gas_duty_cycle, 0., 1., 0., 255.)
-            self.__pi_car.set_PWM_dutycycle(self.__motor_forward_pin, real_pwm)
-
-            if self.__pi_car.read(self.__encoder_dir_pin) == self.__forward_pin_level:
-                real_pwm = self.scale_range(self.__break_duty_cycle, 0., 1., 0., 255.)
-                self.__pi_car.set_PWM_dutycycle(self.__motor_backward_pin, real_pwm)
-            else:
-                self.__pi_car.set_PWM_dutycycle(self.__motor_backward_pin, 0)
-
-        real_pwm = self.scale_range(self.__steering, -1., 1., 1000., 2000.)
-        self.__pi_car.set_servo_pulsewidth(self.__servo_pin, real_pwm)
+        real_pwm = self.scale_range(self.__steering_angle, -1., 1., 1000., 2000.)
+        self.__pi_car.set_servo_pulsewidth(self.__steering_servo_pin, real_pwm)
 
         self.__get_image()
 
-        return self.image, self.__reward, self.__done, self.__info
+        return self.__image, self.__reward, self.__done, self.__info
 
     def close(self) -> None:
-        """Close system.
-
-        Close camera and clear GPIO.
-
-        Returns: None.
-
-        """
         self.__cam.close()
 
-        self.__pi_car.set_PWM_dutycycle(self.__motor_backward_pin, 0)
-        self.__pi_car.set_PWM_dutycycle(self.__motor_forward_pin, 0)
-        self.__pi_car.set_servo_pulsewidth(self.__servo_pin, 0)
+        self.__pi_car.set_servo_pulsewidth(self.__motor, 0)
+        self.__pi_car.set_servo_pulsewidth(self.__steering_servo_pin, 0)
         self.__pi_car.stop()
 
     def __get_image(self) -> None:
-        """Get image from camera.
-
-        Returns: None.
-
-        """
-        self.__cam.capture(self.image, 'rgb', use_video_port=True)
+        self.__cam.capture(self.__image, 'rgb', use_video_port=True)
 
     def __line_interrupt_handle(self) -> None:
-        """Handles ecent from line sensor.
-
-        Triggers done when car hit lines.
-
-        Returns: None.
-
-        """
         self.__done = True
+
+    def __update_reward(self):
+        self.__reward += 5
 
     @staticmethod
     def scale_range(old_value: float, old_min: float, old_max: float, new_min: float, new_max: float) -> float:
