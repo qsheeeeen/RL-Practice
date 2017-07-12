726438826a8e6159dedd29455a0feb92d110497b
Shen Qu <503578404@qq.com>
2017-07-12 17:41:39 +0800

    Fix typo.

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,2 +1,3 @@
 # coding: utf-8
+from .ai_agent import AIAgent
 from .controller_agent import ControllerAgent
---
+++ test/test_ai_agent.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import gym
+
+from agent import AIAgent
+
+
+def main():
+    agent = AIAgent()
+    env = gym.Env()
+
+
+if __name__ == '__main__':
+    main()
