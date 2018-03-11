d1842c832538948be619b89dd77d141cec0a9540
Qu Shen <kshz2155757@gmail.com>
2018-03-11 10:40:40 +0800

    Update shared.py

--- policy/shared.py
+++ policy/shared.py
@@ -8,6 +8,21 @@ import torch.nn.functional as F
 class CNNPolicy(nn.Module):
     def __init__(self, num_outputs):
         super(CNNPolicy, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
         self.conv_1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
         self.bn_1 = nn.BatchNorm2d(16)
         self.conv_2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
