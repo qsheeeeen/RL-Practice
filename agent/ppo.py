469ee267751ae8289f326f03745f3c2d2e7492f5
qushen <kshz2155757@gmail.com>
2018-03-11 21:39:41 +0800

    Change name

--- agent/ppo.py
+++ agent/ppo.py
@@ -19,6 +19,7 @@ class PPOAgent(Agent):
             num_outputs=3,
             horizon=2048,
             lr=3e-4,
+            weight_decay=0.95,
             num_epoch=10,
             batch_size=64,
             discount_factor=0.99,
@@ -28,45 +29,45 @@ class PPOAgent(Agent):
             load=False,
             weight_path='./ppo_weights.pth'):
 
-        self.num_inputs = num_inputs
-        self.horizon = horizon
-        self.lr = lr
-        self.num_epoch = num_epoch
-        self.batch_size = batch_size
-        self.discount_factor = discount_factor
-        self.gae_parameter = gae_parameter
-        self.clip_range = clip_range
-        self.train = train
-        self.load = load
-        self.weight_path = weight_path
+        self._num_inputs = num_inputs
+        self._horizon = horizon
+        self._lr = lr
+        self._num_epoch = num_epoch
+        self._batch_size = batch_size
+        self._discount_factor = discount_factor
+        self._gae_parameter = gae_parameter
+        self._clip_range = clip_range
+        self._train = train
+        self._load = load
+        self._weight_path = weight_path
 
         if num_inputs is not None:
-            self.policy_old = MLPPolicy(num_inputs, num_outputs)
+            self._policy_old = MLPPolicy(num_inputs, num_outputs)
         else:
-            self.policy_old = CNNPolicy(num_outputs)
+            self._policy_old = CNNPolicy(num_outputs)
 
-        self.policy_old.eval()
+        self._policy_old.eval()
 
-        if self.load:
+        if self._load:
             print('Load weights.')
-            self.policy_old.load_state_dict(torch.load(self.weight_path))
+            self._policy_old.load_state_dict(torch.load(self._weight_path))
 
-        if self.train:
-            self.policy = copy.deepcopy(self.policy_old)
-            self.policy.train()
+        if self._train:
+            self._policy = copy.deepcopy(self._policy_old)
+            self._policy.train()
 
-            self.policy_optimizer = Adam(self.policy.parameters(), lr)
+            self._policy_optimizer = Adam(self._policy.parameters(), lr, weight_decay)
 
-            self.replay_buffer = ReplayBuffer(horizon)
+            self._replay_buffer = ReplayBuffer(horizon)
 
-            self.stored = None
+            self._stored = None
 
     def act(self, state_array, reward=0., done=False):
         state = self._preprocessing(state_array)
 
-        mean_var, std_var, value_var = self.policy_old(Variable(state, volatile=True))
+        mean_var, std_var, value_var = self._policy_old(Variable(state, volatile=True))
 
-        if self.train:
+        if self._train:
             m = Normal(mean_var, std_var)
 
             action_var = m.sample()
@@ -75,13 +76,13 @@ class PPOAgent(Agent):
 
             reward = torch.zeros_like(value) + reward
 
-            if self.stored:
-                self.replay_buffer.store(self.stored + [reward])
+            if self._stored:
+                self._replay_buffer.store(self._stored + [reward])
 
-            if len(self.replay_buffer) == self.horizon:
+            if len(self._replay_buffer) == self._horizon:
                 self._finish_iteration()
 
-            self.stored = [state, value, action_var.data]
+            self._stored = [state, value, action_var.data]
 
         else:
             action_var = mean_var
@@ -92,7 +93,7 @@ class PPOAgent(Agent):
         raise NotImplementedError
 
     def save(self):
-        torch.save(self.policy_old.state_dict(), self.weight_path)
+        torch.save(self._policy_old.state_dict(), self._weight_path)
 
     def _preprocessing(self, array):
         try:
@@ -100,13 +101,10 @@ class PPOAgent(Agent):
         except RuntimeError:
             import numpy as np
 
-            a = np.zeros_like(array)
-            a += array
+            a = np.zeros_like(array) + array
             tensor = torch.from_numpy(a).float()
 
-        if self.num_inputs:
-            pass
-        else:
+        if self._num_inputs is None:
             tensor = tensor.permute(2, 0, 1)
             tensor /= 256.
 
@@ -117,13 +115,13 @@ class PPOAgent(Agent):
         advantages[-1] = rewards[-1] - values[-1]
 
         for t in reversed(range(len(rewards) - 1)):
-            delta = rewards[t] + self.discount_factor * values[t + 1] - values[t]
-            advantages[t] = delta + self.discount_factor * self.gae_parameter * advantages[t + 1]
+            delta = rewards[t] + self._discount_factor * values[t + 1] - values[t]
+            advantages[t] = delta + self._discount_factor * self._gae_parameter * advantages[t + 1]
 
         return advantages
 
     def _finish_iteration(self):
-        samples = self.replay_buffer.get_all()
+        samples = self._replay_buffer.get_all()
 
         states, values_old, actions_old, rewards = samples
 
@@ -136,50 +134,45 @@ class PPOAgent(Agent):
         dataset_1 = TensorDataset(states, actions_old)
         dataset_2 = TensorDataset(advantages, values_target)
 
-        data_loader_1 = DataLoader(dataset_1, self.batch_size)
-        data_loader_2 = DataLoader(dataset_2, self.batch_size)
+        data_loader_1 = DataLoader(dataset_1, self._batch_size)
+        data_loader_2 = DataLoader(dataset_2, self._batch_size)
 
-        for _ in range(self.num_epoch):
+        for _ in range(self._num_epoch):
             for (states, actions_old), (advantages, values_target) in zip(data_loader_1, data_loader_2):
                 states_var = Variable(states)
                 actions_old_var = Variable(actions_old)
                 advantages_var = Variable(advantages)
                 values_target_var = Variable(values_target)
 
-                means_old_var, stds_old_var, values_old_var = self.policy_old(states_var)
+                means_old_var, stds_old_var, values_old_var = self._policy_old(states_var)
                 m_old = Normal(means_old_var, stds_old_var)
                 log_probs_old_var = m_old.log_prob(actions_old_var)
 
-                means_var, stds_var, values_var = self.policy(states_var)
+                means_var, stds_var, values_var = self._policy(states_var)
                 m = Normal(means_var, stds_var)
                 log_probs_var = m.log_prob(actions_old_var)
 
                 ratio = torch.exp(log_probs_var - log_probs_old_var)
 
                 surrogate_1 = ratio * advantages_var
-                surrogate_2 = torch.clamp(ratio, 1.0 - self.clip_range, 1.0 + self.clip_range) * advantages_var
+                surrogate_2 = torch.clamp(ratio, 1.0 - self._clip_range, 1.0 + self._clip_range) * advantages_var
                 pessimistic_surrogate = -torch.mean(torch.min(surrogate_1, surrogate_2))
 
                 value_loss = torch.mean(torch.pow((values_var - values_target_var), 2))
 
                 total_loss = pessimistic_surrogate + value_loss
 
-                self.policy_optimizer.zero_grad()
+                self._policy_optimizer.zero_grad()
                 total_loss.backward()
-                self.policy_optimizer.step()
+                self._policy_optimizer.step()
 
-        self.policy_old.load_state_dict(self.policy.state_dict())
+        self._policy_old.load_state_dict(self._policy.state_dict())
 
 
 if __name__ == '__main__':
     import gym
     import time
     import numpy as np
-
-
-    def fun(x):
-        return np.minimum(np.maximum(x, -1), 1)
-
 
     env = gym.make('CarRacing-v0')
     inputs = env.observation_space.shape[0]
@@ -189,16 +182,14 @@ if __name__ == '__main__':
         ob = env.reset()
         env.render()
         action = agent.act(ob)
-        # action = fun(action)
         for x in range(10000):
             ob, r, d, _ = env.step(action)
             env.render()
             action = agent.act(ob, r, d)
-            # action = fun(action)
             if d:
                 print()
                 print(time.ctime())
                 print('Done i:{},x:{} '.format(i, x))
                 d = False
-                agent.save()
+                # agent.save()
                 break
