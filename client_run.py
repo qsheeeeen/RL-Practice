39472a1a4c09eb896f5353744de6f95de96213c7
qushen <kshz2155757@gmail.com>
2018-03-27 16:30:33 +0800

    wait to test whole communication system.

--- client_run.py
+++ client_run.py
@@ -1,12 +1,10 @@
+import gym
+
 from .self_driving_car.tool import Com
 
 
 def main():
     client = Com('client', '127.0.0.1')
-
-    # env = RacingCar()
-
-    import gym
     env = gym.make('CarRacing-v0')
 
     data = env.reset()
--- server_run.py
+++ server_run.py
@@ -1,13 +1,23 @@
-from .self_driving_car.agent import PPOAgent, JoystickAgent
+import gym
+
+from .self_driving_car.agent import PPOAgent
 from .self_driving_car.policy.shared import CNNPolicy
 from .self_driving_car.tool import Com, Dashboard
 
 
 def main():
     server = Com('server')
-    # agent = PPOAgent(CNNPolicy, )
-    agent = JoystickAgent((96, 96, 3), (2,))
-    dashboard = Dashboard()
+
+    env = gym.make('CarRacing-v0')
+
+    inputs = env.observation_space.shape
+    outputs = env.action_space.shape
+
+    del env
+
+    agent = PPOAgent(CNNPolicy, inputs, outputs, output_limit=(-1, 1), load=True)
+
+    # dashboard = Dashboard()
 
     while True:
         data = server.receive_data()
@@ -16,7 +26,7 @@ def main():
 
         server.send_data(action)
 
-        dashboard.update(data[0], data[-1])
+        # dashboard.update(data[0], data[-1])
 
 
 if __name__ == '__main__':
