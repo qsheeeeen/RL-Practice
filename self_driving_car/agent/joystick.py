Fix joystick agent bug. Add average fps to dashboard.

self_driving_car/agent/joystick.py
@@ -67,7 +67,7 @@ class JoystickAgent(object):
         gas_signal = (gas_signal + 1) / 2
 
         break_signal = self.joystick.get_axis(self.BREAK_AXIS)
-        break_signal = (break_signal + 1) / 2
+        break_signal = -(break_signal + 1) / 2
 
         if self.output_shape[0] == 3:
             action_array = np.array((steering, gas_signal, break_signal), dtype=np.float32)

self_driving_car/environment/racing_car.py
@@ -110,14 +110,20 @@ class RacingCar(object):
         self.cam.capture(self.image, 'rgb', use_video_port=True)
 
     def _interrupt_handle(self, gpio, level, tick):
+        print('Interrupt trigered.')
+        print('gpio: {}\t level: {}'.format(gpio, level))
+
         if (gpio == self.LEFT_LINE_SENSOR_PIN) or (gpio == self.RIGHT_LINE_SENSOR_PIN):
+            print('Done trigered.')
             self.done = True
 
         elif gpio == self.ENCODER_PUL_PIN:
             if level == pigpio.RISING_EDGE:
+                print('pulse count +1.')
                 self.encoder_pulse_count += 1
 
             elif level == pigpio.TIMEOUT:
+                print('Calculate speed.')
                 s = self.encoder_pulse_count / self.ENCODER_LINE * np.pi * self.TIRE_DIAMETER
                 t = self.SPEED_UPDATE_INTERVAL / 1000
                 self.speed = s / t * self.GEAR_RATIO
@@ -128,9 +134,11 @@ class RacingCar(object):
 
         elif gpio == self.ENCODER_ZERO_PIN:
             if level == pigpio.RISING_EDGE:
-                self.total_reward += 5
+                print('total_reward += 5.')
+                self.total_reward += 500
 
             elif level == pigpio.TIMEOUT:
+                print('total_reward -= 1.')
                 self.total_reward -= 1
 
             else:

self_driving_car/tool/dashboard.py
@@ -1,5 +1,7 @@
 import time
+from collections import deque
 
+import numpy as np
 import pygame
 
 
@@ -21,10 +23,11 @@ class Dashboard(object):
         self.screen.fill(self.WHITE)
         self.font = pygame.font.Font(None, self.FRONT_SIZE)
 
+        self.buffer = deque(maxlen=100)
+
         self.last_time = time.time()
 
     def update(self, image, info=None):
-        y_position = 10
         self.screen.fill(self.WHITE)
 
         image_surface = pygame.surfarray.make_surface(image)
@@ -36,15 +39,26 @@ class Dashboard(object):
         current_time = time.time()
         fps = 1. / (current_time - self.last_time)
         self.last_time = current_time
-        self._screen_print('FPS:', [self.INFO_KET_LEFT, y_position])
+
+        self.buffer.append(fps)
+        average_fps = np.mean(self.buffer)
+
+        y_position = 10
+        self._screen_print('fps:', [self.INFO_KET_LEFT, y_position])
         self._screen_print(fps, [self.INFO_VALUE_LEFT, y_position])
 
         y_position += self.FRONT_SIZE
+        self._screen_print('average fps:', [self.INFO_KET_LEFT, y_position])
+        self._screen_print(average_fps, [self.INFO_VALUE_LEFT, y_position])
+
+        y_position += self.FRONT_SIZE
         for head in info.keys():
+            if isinstance(head, bytes):
+                head = head.decode()
             self._screen_print(head + ':', [self.INFO_KET_LEFT, y_position])
             y_position += self.FRONT_SIZE
 
-        y_position = 10
+        y_position = 10 + self.FRONT_SIZE
         y_position += self.FRONT_SIZE
         for data in info.values():
             self._screen_print(data, [self.INFO_VALUE_LEFT, y_position])