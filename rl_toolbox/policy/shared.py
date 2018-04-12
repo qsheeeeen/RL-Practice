443d79c53396f4302a3c7b12bceec4b749541b8e
qushen <kshz2155757@gmail.com>
2018-04-12 21:35:14 +0800

    Structure change. Code cleaned.

--- rl_toolbox/policy/shared.py
+++ rl_toolbox/policy/shared.py
@@ -18,7 +18,7 @@ class CNNPolicy(nn.Module):
         size = self.cnn.fc.out_features
 
         self.mean_fc = nn.Linear(size, output_shape[0])
-        self.std = nn.Parameter(torch.zeros(output_shape[0]))
+        self.log_std = nn.Parameter(torch.zeros(output_shape[0]))
         self.value_fc = nn.Linear(size, 1)
 
         self.apply(orthogonal_init([nn.Linear], 'linear'))
@@ -31,15 +31,15 @@ class CNNPolicy(nn.Module):
         feature = self.cnn(x)
 
         mean = self.mean_fc(feature)
-        value = self.value_fc(feature)
-
-        log_std = self.std.expand_as(mean)
+        log_std = self.log_std.expand_as(mean)
         std = torch.exp(log_std)
+
+        value = self.value_fc(feature)
 
         return mean, std, value
 
 
-class CNNLSTMPolicy(nn.Module):  # TODO(1st): How to train.
+class CNNLSTMPolicy(nn.Module):
     def __init__(self, input_shape, output_shape):
         super(CNNLSTMPolicy, self).__init__()
         self.recurrent = True
@@ -51,7 +51,7 @@ class CNNLSTMPolicy(nn.Module):  # TODO(1st): How to train.
         self.rnn = SmallRNN(size, size)
 
         self.mean_fc = nn.Linear(size, output_shape[0])
-        self.std = nn.Parameter(torch.ones(output_shape[0]))
+        self.log_std = nn.Parameter(torch.ones(output_shape[0]))
         self.value_fc = nn.Linear(size, 1)
 
         self.apply(orthogonal_init([nn.Linear], 'linear'))
@@ -61,8 +61,6 @@ class CNNLSTMPolicy(nn.Module):  # TODO(1st): How to train.
         self.float()
         self.cuda()
 
-        self.rnn.rnn.flatten_parameters()
-
     def forward(self, x):
         feature = self.cnn(x)
 
@@ -71,7 +69,7 @@ class CNNLSTMPolicy(nn.Module):  # TODO(1st): How to train.
         mean = self.mean_fc(memory)
         value = self.value_fc(memory)
 
-        log_std = self.std.expand_as(mean)
+        log_std = self.log_std.expand_as(mean)
         std = torch.exp(log_std)
 
         return mean, std, value
@@ -113,17 +111,17 @@ class MLPPolicy(nn.Module):
         return mean, std, value
 
 
-class MLPLSTMPolicy(nn.Module):
+class MLPLSTMPolicy(nn.Module):  # TODO: Try single rnn layer
     def __init__(self, input_shape, output_shape):
         super(MLPLSTMPolicy, self).__init__()
         self.recurrent = True
         self.pd_fn = Normal
 
-        self.pi_fc1 = nn.Linear(input_shape[0], 64)
-        self.pi_rnn = SmallRNN(64, 64)
+        self.pi_rnn = SmallRNN(input_shape[0], 64)
+        self.pi_fc = nn.Linear(64, 64)
 
-        self.vf_fc1 = nn.Linear(input_shape[0], 64)
-        self.vf_rnn = SmallRNN(64, 64)
+        self.vf_rnn = SmallRNN(input_shape[0], 64)
+        self.vf_fc = nn.Linear(64, 64)
 
         self.mean_fc = nn.Linear(64, output_shape[0])
         self.log_std = nn.Parameter(torch.zeros(output_shape[0]))
@@ -135,15 +133,15 @@ class MLPLSTMPolicy(nn.Module):
         self.cuda()
 
     def forward(self, x):
-        pi_h1 = F.tanh(self.pi_fc1(x))
-        pi_h2 = F.tanh(self.pi_rnn(pi_h1))
+        pi_h1 = F.tanh(self.pi_rnn(x))
+        pi_h2 = F.tanh(self.pi_fc(pi_h1))
 
         mean = self.mean_fc(pi_h2)
         log_std = self.log_std.expand_as(mean)
         std = torch.exp(log_std)
 
-        vf_h1 = F.tanh(self.vf_fc1(x))
-        vf_h2 = F.tanh(self.vf_rnn(vf_h1))
+        vf_h1 = F.tanh(self.vf_rnn(x))
+        vf_h2 = F.tanh(self.vf_fc(vf_h1))
         value = self.value_fc(vf_h2)
 
         return mean, std, value
