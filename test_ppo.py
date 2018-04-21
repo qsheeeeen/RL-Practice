a98064533df96308dbafc005e6527faa6c05fd0c
qushen <kshz2155757@gmail.com>
2018-04-21 13:50:55 +0800

    Cleaner code.

--- test_ppo.py
+++ test_ppo.py
@@ -1,24 +1,20 @@
 from rl_toolbox import Runner
 from rl_toolbox.agent import PPOAgent
-from rl_toolbox.policy import MLPPolicy
-
-
-def policy_fn(policy):
-    return PPOAgent(policy, train=True)
+from rl_toolbox.policy import MLPLSTMPolicy
 
 
 def main():
     runner = Runner(
         'LunarLanderContinuous-v2',
-        policy_fn,
-        MLPPolicy,
+        PPOAgent,
+        MLPLSTMPolicy,
         record_data=False,
         data_path=None,
         save=True,
         load=False,
         weight_path='./weights/')
 
-    runner.run(num_episode=500, num_worker=2)
+    runner.run(num_episode=1000, num_worker=1)
 
 
 if __name__ == '__main__':
